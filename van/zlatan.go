ee2f9ddfcfc07f4349cea249d3dc7943ba38664c
Gavin Bong <rubycoder@gmail.com>
2014-04-05 20:28:55 +0000

    b4 lunch

--- van/zlatan.go
+++ van/zlatan.go
@@ -1,2 +1,3 @@
--from 00:00:44 -length 15s -fps 17 -speed=faster
-    -scale 320:_ // HLspe
+seneca -video-infile=~/zlatan-at-ajax.mp4 
+       -from 00:00:44 -length 15s -fps 17 -speed=faster
+       -scale 320:_ // HLspe
