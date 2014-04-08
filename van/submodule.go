fe7d980729f5cde86e46848189c3db13fb2b37ff
Gavin Bong <rubycoder@gmail.com>
2014-04-08 08:09:24 +0000

    tidur

---
+++ van/submodule.go
@@ -0,0 +1,9 @@
+$ git clone --recursive git@github.com:javouhey/seneca.git   // HL
+  Submodule 'vendor/github.com/stretchr/testify'
+  (https://github.com/stretchr/testify.git) registered for path
+                           'vendor/github.com/stretchr/testify'
+  Cloning into 'vendor/github.com/stretchr/testify'...
+  ...
+  Submodule path 'vendor/github.com/stretchr/testify': checked out
+                        '6b8df2220f16006bdffc26b3fc73ce0cc3feefe1'  // HL
+
---
+++ van/versioning.go
@@ -0,0 +1,7 @@
+
+If you depend on any third party library you are risking      // HL
+that this library is broken, depends on stuff you don't like, // HL
+won't compile, is evil, becomes unmaintained, gets deprecated // HL
+by the vendor, is ruled to be illegal or gets changed in ways // HL
+unsuitable to your need.  // HL
+    - Volker Dobler
